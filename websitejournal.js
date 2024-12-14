96fdc27281d9104b4bd40b3879b2dcd4f88829e4
Jakmobs <jakmobs@gmail.com>
2024-12-14 11:21:58 +0800

    Add files via upload

---
+++ websitejournal.js
@@ -0,0 +1,23 @@
+function church(){
+    window.location.href = "church.html"
+}
+
+function homeButton(){
+    window.location.href = "index.html"
+}
+
+function classroomButton(){
+    window.location.href = "classroom.html"
+}
+
+function courtButton(){
+    window.location.href = "court.html"
+}
+
+function communityButton(){
+    window.location.href = "community.html"
+}
+
+function cultureButton(){
+    window.location.href = "culture.html"
+}
